9953fb269b8d0df07a1524da2f78a59d3a3fd09f
AceHotel33 <lomvax@gmail.com>
2024-09-22 13:08:36 +0300

    middlewareMetricsIInc ini progress

--- main.go
+++ main.go
@@ -5,15 +5,37 @@ import (
 	"net/http"
 )
 
+type apiConfig struct {
+	fileserverHits int
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	cfg.fileserverHits += 1
+	return next
+}
+
+func (cfg *apiConfig) serverHitsHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hits: %d", cfg.fileserverHits)
+}
+
+var appHandler http.Handler = http.StripPrefix("/app", http.FileServer(http.Dir(".")))
+
+var assetsHandler http.Handler = http.FileServer(http.Dir("./assets"))
+
+func healthzFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
 func main() {
+	apiCfg := apiConfig{}
 
 	mux := http.NewServeMux()
-
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
-
-	mux.Handle("/assets", http.FileServer(http.Dir("./assets")))
-
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(appHandler))
+	mux.Handle("/assets", assetsHandler)
 	mux.Handle("/healthz", http.HandlerFunc(healthzFunc))
+	mux.HandleFunc("/metrics", apiCfg.serverHitsHandler)
 
 	server := &http.Server{
 		Addr:    "localhost:8080",
@@ -24,10 +46,4 @@ func main() {
 		fmt.Printf("error listening and serving - %v", err)
 	}
 
-}
-
-func healthzFunc(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("OK"))
 }
